847e520d695f228c78c1baa2024561fd079f9eed
Eduardo Miguel Alves Nascimento <edunascimentoalvess@dinfor.uniceub>
2025-02-27 20:48:26 -0300

    Create CambioExternalService

--- infra/externalServices/CambioExternalService.cs
+++ infra/externalServices/CambioExternalService.cs
@@ -1,6 +1,38 @@
-﻿namespace TravelerFellas;
+﻿
+using System.Text.Json;
 
-public class CambioExternalService
+namespace TravelerFellas;
+
+public class CambioExternalService : ICambioExternalServices
 {
 
+    private static readonly HttpClient httpClient = new()
+    {
+        BaseAddress = new Uri("https://economia.awesomeapi.com.br"),
+    };
+
+
+    public async Task<Response<CambioDTO>> CreateCambioAsync(string deMoeda, string paraMoeda, Request request)
+    {
+        string uri = ($"{deMoeda}-{paraMoeda}/1");
+        var response = await httpClient.GetAsync(uri);
+
+        response.EnsureSuccessStatusCode();
+
+        var json = await response.Content.ReadAsStringAsync();
+
+        var data = JsonSerializer.Deserialize<JsonElement[]>(json);
+        decimal valor = data[0].GetProperty("high").GetDecimal();
+
+
+        var cambioDTO = new CambioDTO()
+        {
+            MoedaOrigem = deMoeda,
+            MoedaDestino = paraMoeda,
+            TaxaConversao = valor,
+            DataAtualizacao = DateTime.Now
+        };
+
+        return new Response<CambioDTO> ( 200, "Cambio obtido com sucesso", cambioDTO );
+    }
 }
